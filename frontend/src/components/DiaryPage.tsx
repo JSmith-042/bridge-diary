a150cc1a2072075dafa6a9c89321418e53398dd2
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-22 17:23:18 -0500

    build dairy entry dates display from diary data

--- frontend/src/components/DiaryPage.tsx
+++ frontend/src/components/DiaryPage.tsx
@@ -1,13 +1,23 @@
 import DateDisplaySelector from "./DateDisplaySelector";
 import DiaryEntryDisplay from "./DiaryEntryDisplay";
+import {useEffect, useState} from "react";
+import {getListOfEntries} from "../services/DiaryService";
+import {DiaryEntry} from "../types/DiaryEntry";
+
 
 const DiaryPage = () => {
+    const [entryList, setEntryList] = useState([{id:-1, title:"", text:"", date: new Date("0000-00-00")}])
+
+    useEffect(() => {
+        getListOfEntries().then((r:DiaryEntry[])=>{})
+    }, []);
+
     return (
         <>
             <div className="text-center"><h1>Welcome to your personal diary</h1></div>
             <div className={"grid grid-flow-col gap-4"}>
                 <div className="col-span-1 row-span-full h-screen">
-                    <DateDisplaySelector></DateDisplaySelector>
+                    <DateDisplaySelector dates={entryList.filter((entry:DiaryEntry) => (entry.date)).map((entry:DiaryEntry)=>(entry.date))}></DateDisplaySelector>
                 </div>
                 <div className="col-span-2 row-span-full">
                     <DiaryEntryDisplay title={""} text={""}></DiaryEntryDisplay>
--- frontend/src/components/__tests__/DiaryPage.test.tsx
+++ frontend/src/components/__tests__/DiaryPage.test.tsx
@@ -1,18 +1,35 @@
-import {render, screen} from '@testing-library/react'
-import {describe, expect, it} from "vitest";
+import {render, screen, waitFor} from '@testing-library/react'
+import {describe, expect, it, vi} from "vitest";
 import DiaryPage from "../DiaryPage";
+import * as diaryService from "../../services/DiaryService";
+import {DiaryEntry} from "../../types/DiaryEntry";
 
 describe('Diary Page', ()=>
 {
-    it('should display welcome header', () => {
-        render(<DiaryPage/>)
+    const doRender = async () => await waitFor(()=>{render(<DiaryPage/>)})
+
+    it('should display welcome header', async () => {
+        await doRender();
 
         const welcomeHeader = screen.getByRole("heading", {name:"Welcome to your personal diary"})
 
         expect(welcomeHeader).toBeVisible();
     });
 
-    it('should display diary entry dates', () => {
+    it('should display diary entry dates', async () => {
+        const testEntries:DiaryEntry[] =
+            [{id:0, title:'test1', text:"text1", date: new Date("2025-02-05")},
+            {id:1, title:'test1', text:"text1", date: new Date("2025-03-06")},
+            {id:2, title:'test1', text:"text1", date: new Date("2025-01-07")}];
+
+        const getEntries = vi.spyOn(diaryService, "getListOfEntries").mockResolvedValueOnce(testEntries)
+        await doRender();
+
+
+        const dateList = await screen.findAllByRole("listitem");
+
+
+        expect(getEntries).toHaveBeenCalledOnce();
 
     });
 })
