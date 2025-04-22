1ca1e2ba8bee3205c9bd3aba71f8d1f88c0d8058
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-22 13:44:23 -0500

    build out diary entry input form

--- frontend/src/components/DiaryInputForm.tsx
+++ frontend/src/components/DiaryInputForm.tsx
@@ -1,8 +1,34 @@
 const DiaryInputForm = () => {
     return (
-        <div>
-            
-        </div>
+        <form className="max-w-sm mx-auto grid gap-4 mx:gap-6">
+            <div className={"mx:grid-cols-6"}>
+                <label role={"contentinfo"} aria-label={"titleLabel"}>Title:
+                    <input type={"text"}
+                           className={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}/>
+                </label>
+                <label role={"contentinfo"} aria-label={"dateLabel"}>Date:
+                    <input type={"date"}
+                           className={"dark:[color-scheme:dark] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}
+                           defaultValue={new Date().toLocaleDateString('en-CA')}
+                    />
+                </label>
+            </div>
+            <label role={"contentinfo"} aria-label={"entryLabel"}>Entry:
+                <textarea
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50
+                          rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500
+                          dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                          dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    // style={{resize:"none", height:"auto", sc}}
+                ></textarea>
+            </label>
+
+            <button type="submit"
+                    className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+
+            >Add Entry
+            </button>
+        </form>
     );
 };
 
--- frontend/src/components/__tests__/DiaryInputForm.test.tsx
+++ frontend/src/components/__tests__/DiaryInputForm.test.tsx
@@ -1,11 +1,52 @@
 import {render, screen} from '@testing-library/react'
-import {describe, it} from "vitest";
+import {describe, expect, it} from "vitest";
 import DiaryInputForm from "../DiaryInputForm.tsx";
 
 describe('Diary Input Form', ()=>
 {
-    it('should', () => {
+    it('should display "Title: " label with title input field', () => {
         render(<DiaryInputForm></DiaryInputForm>)
 
+        const titleLabel = screen.getByRole('contentinfo', {name: "titleLabel"});
+        const titleInput = screen.getByLabelText("Title:");
+
+
+        expect(titleLabel).toBeVisible();
+        expect(titleInput).toBeVisible();
     });
+
+    it('should display "Date: " label with date picker input field with initial value of current date', () => {
+        render(<DiaryInputForm></DiaryInputForm>)
+
+        const dateLabel = screen.getByRole('contentinfo', {name: "dateLabel"})
+        const dateInput = screen.getByLabelText("Date:");
+
+        expect(dateLabel).toBeVisible();
+        expect(dateInput).toBeVisible();
+
+        expect(dateInput).toHaveValue(new Date().toLocaleDateString('en-CA'))
+    });
+
+    it('should display "Entry: " label with entry textarea input', () => {
+        render(<DiaryInputForm></DiaryInputForm>)
+
+        const entryLabel = screen.getByRole('contentinfo', {name: "entryLabel"});
+        const entryInput = screen.getByLabelText("Entry:");
+
+
+        expect(entryLabel).toBeVisible();
+        expect(entryInput).toBeVisible();
+
+
+
+    });
+
+    it('should display Add Button', () => {
+        render(<DiaryInputForm></DiaryInputForm>)
+
+        const addButton = screen.getByRole("button", {name: "Add Entry"});
+
+        expect(addButton).toBeVisible();
+    });
+
 })
