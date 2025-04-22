284742efc70cf4ee242f4d29d62c6c9f8ac572bd
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-22 14:24:38 -0500

    basic page layout

--- frontend/src/components/DiaryPage.tsx
+++ frontend/src/components/DiaryPage.tsx
@@ -1,9 +1,17 @@
 const DiaryPage = () => {
     return (
-        <div>
-            
-        </div>
-    );
+        <>
+            <div className="text-center"><h1>Welcome to your personal diary</h1></div>
+            <div className={"grid grid-flow-col gap-4"}>
+                <div className="col-span-1 row-span-full h-screen">
+
+                </div>
+                <div className="col-span-2 row-span-full">
+
+                </div>
+            </div>
+        </>
+    )
 };
 
 export default DiaryPage;
--- frontend/src/components/__tests__/DiaryPage.test.tsx
+++ frontend/src/components/__tests__/DiaryPage.test.tsx
@@ -1,7 +1,16 @@
 import {render, screen} from '@testing-library/react'
 import {describe, expect, it} from "vitest";
+import DiaryPage from "../DiaryPage";
 
 describe('Diary Page', ()=>
 {
+    it('should display welcome header', () => {
+        render(<DiaryPage/>)
 
+        const welcomeHeader = screen.getByRole("heading", {name:"Welcome to your personal diary"})
+
+        expect(welcomeHeader).toBeVisible();
+
+
+    });
 })
