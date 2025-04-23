7a615ac8e03fb1277d2d4b63ff02813a5d71a5a2
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-23 15:33:56 -0500

    entry by id and save entry buildout

--- backend/src/main/java/jsbrg/backend/service/DiaryEntryService.java
+++ backend/src/main/java/jsbrg/backend/service/DiaryEntryService.java
@@ -5,6 +5,7 @@ import jsbrg.backend.repository.DiaryEntryRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DiaryEntryService {
@@ -18,5 +19,14 @@ public class DiaryEntryService {
 
     public List<DiaryEntryEntity> findAllEntries() {
         return diaryEntryRepo.findAll();
+    }
+
+    public DiaryEntryEntity findEntryById(Long id) {
+        Optional<DiaryEntryEntity> entity = diaryEntryRepo.findById(id);
+        return entity.orElse(null);
+    }
+
+    public DiaryEntryEntity save(DiaryEntryEntity testEntityNoId) {
+        return diaryEntryRepo.save(testEntityNoId);
     }
 }
--- backend/src/test/java/jsbrg/backend/service/DiaryEntryServiceTest.java
+++ backend/src/test/java/jsbrg/backend/service/DiaryEntryServiceTest.java
@@ -13,6 +13,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -29,6 +30,7 @@ public class DiaryEntryServiceTest {
     List<DiaryEntryEntity> testEntryList;
     DiaryEntryEntity testEntity;
     DiaryEntryEntity testEntity2;
+    DiaryEntryEntity testEntityNoId;
 
     @BeforeEach
     void setUp() throws ParseException {
@@ -38,6 +40,8 @@ public class DiaryEntryServiceTest {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         testEntity = new DiaryEntryEntity("my thoughts", "my text", sdf.parse("02-02-2025").toInstant(), 3, .25);
         testEntity.setId(1L);
+
+        testEntityNoId = new DiaryEntryEntity("my thoughts", "my text", sdf.parse("02-02-2025").toInstant(), 3, .25);
 
         testEntity2 = new DiaryEntryEntity("my second thoughts", "you can't read me", sdf.parse("01-02-2025").toInstant(), 2, .75);
         testEntity2.setId(2L);
@@ -54,4 +58,22 @@ public class DiaryEntryServiceTest {
         verify(diaryEntryRepo, times(1)).findAll();
         assertThat(listOfEntryRequest).isEqualTo(testEntryList);
     }
+
+    @Test
+    void shouldGetEntryById() {
+        when(diaryEntryRepo.findById(1L)).thenReturn(Optional.of(testEntity));
+        DiaryEntryEntity actualRequest = diaryEntryService.findEntryById(1L);
+        verify(diaryEntryRepo, times(1)).findById(1L);
+        assertThat(actualRequest).isEqualTo(testEntity);
+    }
+
+    @Test
+    void shouldSaveEntry() {
+        when(diaryEntryRepo.save(testEntityNoId)).thenReturn(testEntity);
+        DiaryEntryEntity actualRequest = diaryEntryService.save(testEntityNoId);
+        verify(diaryEntryRepo, times(1)).save(testEntityNoId);
+        assertThat(actualRequest).isEqualTo(testEntity);
+    }
+
+
 }
