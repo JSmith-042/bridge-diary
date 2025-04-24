f2fe85047e421c7a2c87c0957d103aec82218d40
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-24 14:23:39 -0500

    update vite config for proxy server configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,19 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port:3000,
+    strictPort: true,
+    hmr:{
+      clientPort: 3000,
+    },
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     outDir: 'build',
   },
@@ -14,5 +27,5 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: "./src/setupTests.ts",
     css: true
-  }
+  },
 })
